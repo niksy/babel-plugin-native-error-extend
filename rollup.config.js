296075d3be515a89e8b8e6ed3b57e3a5fd77b6ae
Ivan Nikolić <niksy5@gmail.com>
2020-11-06 15:17:42 +0100

    Upgrade package
    Closes #1.

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,53 @@
 'use strict';
 
-const babel = require('rollup-plugin-babel');
+const path = require('path');
+const { promises: fs } = require('fs');
+const { default: babel } = require('@rollup/plugin-babel');
 
 module.exports = {
 	input: 'index.js',
 	output: [
 		{
-			file: 'index.cjs.js',
+			file: 'cjs/index.js',
 			format: 'cjs',
+			exports: 'auto',
 			sourcemap: true
 		},
 		{
-			file: 'index.esm.js',
+			file: 'esm/index.js',
 			format: 'esm',
 			sourcemap: true
 		}
 	],
 	plugins: [
+		(() => {
+			return {
+				name: 'package-type',
+				async writeBundle(output) {
+					let prefix, type;
+					if (output.file.includes('cjs/')) {
+						prefix = 'cjs';
+						type = 'commonjs';
+					} else if (output.file.includes('esm/')) {
+						prefix = 'esm';
+						type = 'module';
+					}
+					if (typeof prefix !== 'undefined') {
+						const package_ = path.join(prefix, 'package.json');
+						try {
+							await fs.unlink(package_);
+						} catch (error) {}
+						await fs.writeFile(
+							package_,
+							JSON.stringify({ type }),
+							'utf8'
+						);
+					}
+				}
+			};
+		})(),
 		babel({
+			babelHelpers: 'bundled',
 			exclude: 'node_modules/**'
 		})
 	]
